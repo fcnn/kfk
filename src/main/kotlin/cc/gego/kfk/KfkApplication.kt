b94534a81eab06f4f3257d50abf6cc5cf82dcfb8
i <i@iin.im>
2018-01-05 01:33:10 +0800

    .

--- src/main/kotlin/cc/gego/kfk/KfkApplication.kt
+++ src/main/kotlin/cc/gego/kfk/KfkApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RobotApplication
+class KfkApplication
 
 fun main(args: Array<String>) {
-    runApplication<RobotApplication>(*args)
+    runApplication<KfkApplication>(*args)
 }
